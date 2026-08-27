a9c28eaf5d5f6eeece7d3ee1950c3dc7492cfc3d
João Viana <57032457+vollous@users.noreply.github.com>
2026-08-27 10:31:18 +0100

    refactor chat

---
+++ Project_RAG/chat.py
@@ -0,0 +1,26 @@
+import requests
+
+class Chat:
+    def __init__(self, name:str, rag:bool):
+        self.name = name
+        self.rag = rag
+        self.history = []
+
+    def chat(self, message):
+        self.history.append({"role": "user", "content": message})
+        self.history.append({"role": "assistant", "content": 2})
+        return "2"
+        r = requests.post(
+                "http://127.0.0.1:8000/chat/",
+                json={"question": message},
+                timeout=300,     
+            )
+        
+        r.raise_for_status()
+        data = r.json()
+        response = data["response"]["message"]["content"]
+        self.history.append({"role": "assistant", "content": response})
+        return(response)
+
+    def clear_chat(self):
+        self.history.clear()
